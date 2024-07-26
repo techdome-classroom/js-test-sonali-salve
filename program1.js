f87e819bb1a50371d17ee3026678ffa30fab7d9d
sonali salve <sonalisalve10012002@gmail.com>
2024-07-26 12:45:54 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,6 +1,6 @@
 const getTotalIsles = function (grid,x,y,i,j,v) {
 
-  const row =grid.length;
+  const rows=grid.length;
   const cols =grid[0];
   if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] <= 0) {
     return;
