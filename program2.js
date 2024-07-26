1a80390430afb48be369f7694ab28851b44db13c
sonali salve <sonalisalve10012002@gmail.com>
2024-07-26 13:22:29 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,6 +1,29 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  function decodeTheRing(input, pattern) {
+    let i = 0, j = 0;
+
+    while (i < input.length && j < pattern.length) {
+        if (pattern[j] === '*') {
+            j++;
+            if (j === pattern.length) return true; // '*' at the end matches any suffix
+            while (i < input.length && input[i] !== pattern[j]) {
+                i++;
+            }
+        } else if (pattern[j] !== input[i]) {
+            return false;
+        } else {
+            i++;
+            j++;
+        }
+    }
+
+    // If we reached the end of both strings, it's a match
+    return i === input.length && j === pattern.length;
+}
+
+module.exports = decodeTheRing;
+
 
   };
   
